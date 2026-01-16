6bdbed6fad88a0092f4fd5ba31ef55bf501beb6b
serafimmatheus <matheus18serafim@gmail.com>
2026-01-16 09:03:47 -0300

    feat: Create guest layout and test view for initial application structure, including dynamic title and asset links.

---
+++ resources/views/teste.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.guest-layout>
+    <div>
+        Hello World
+    </div>
+</x-layouts.guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // Retornar a view teste.blade.php
+    return view('teste');
 })->name('home');
